7133ceeed8967c69904a2d6f2eb016da80a255f9
yfning <yfning@imail.isoftstone.com>
2017-12-23 21:28:45 +0800

    Initial Commit

---
+++ PodTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PodTest
+//
+//  Created by nyf on 2017/12/23.
+//  Copyright © 2017年 RT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PodTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PodTest
+//
+//  Created by nyf on 2017/12/23.
+//  Copyright © 2017年 RT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
